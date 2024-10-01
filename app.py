096949e462e2b086e05a8cd4fdc829c0568a6d26
GustavoLOliveiraOt <136750644+GustavoLOliveiraOt@users.noreply.github.com>
2024-10-01 11:56:08 -0300

    organização do codigo

--- app.py
+++ app.py
@@ -24,67 +24,67 @@ cache = Cache(app, config={'CACHE_TYPE': 'simple'})  # Use um cache simples em m
 @cache.cached(timeout=600)  # Cache válido por 10 minutos (ajuste conforme necessário)
 def get_stock_info(symbol):
     try:
-        # Ajuste o símbolo
-        # adjusted_symbol = adjust_symbol(symbol)
         stock = yf.Ticker(symbol)
         info = stock.info
 
-        # Margem de lucro
-        if 'profitMargins' in info:
-            margemDeLucro = info['profitMargins']
-        else:
-            margemDeLucro = 0
-
-        # dividendYield
-        if 'dividendYield' in info:
-            dividendosPorAno = info['dividendYield']
-        else:
-            dividendosPorAno = 0
-
-        # Valor Patrimonial por ação
-        if 'bookValue' in info:
-            valorPatrimonialPorAcao = info['bookValue']
-        else:
-            valorPatrimonialPorAcao = 0
-
-        # Divida total
-        if 'totalDebt' in info:
-            dividaLiquida = info['totalDebt']
-        else:
-            dividaLiquida = 9999999999
-
-        # Preço da ação
-        if 'currentPrice' in info:
-            precoAtual = info['currentPrice']
-        else:
-            precoAtual = 0
-
-        # Patrimonio total
-        if 'sharesOutstanding' in info:
-            patrimonio = precoAtual * info['sharesOutstanding']
-        else:
-            patrimonio = 0
-
-        # Indice de endividamento
-        if 'totalDebt' in info and patrimonio > 0:
-            indiceDeEndividamento = dividaLiquida / patrimonio
-        else:
-            indiceDeEndividamento = 0
-
-        # Retorno sobre Ativos (ROA)
-        if 'returnOnAssets' in info:
-            retornoSobreAtivos = info['returnOnAssets']
-        else:
-            retornoSobreAtivos = 0
-
-
-        dados = np.array([margemDeLucro,dividendosPorAno,valorPatrimonialPorAcao,dividaLiquida,precoAtual,patrimonio,indiceDeEndividamento,retornoSobreAtivos])
+        dados = get_infos(info)
         dados_scaled = scaler.transform([dados])
         previsao = modelo.predict(dados_scaled)
 
         return jsonify({'recomendado': int(previsao[0])})
     except Exception as e:
         return jsonify({'error': str(e)})
+
+def get_infos(stock_info):
+    # Margem de lucro
+    if 'profitMargins' in stock_info:
+        margemDeLucro = stock_info['profitMargins']
+    else:
+        margemDeLucro = 0
+
+    # dividendYield
+    if 'dividendYield' in stock_info:
+        dividendosPorAno = stock_info['dividendYield']
+    else:
+        dividendosPorAno = 0
+
+    # Valor Patrimonial por ação
+    if 'bookValue' in stock_info:
+        valorPatrimonialPorAcao = stock_info['bookValue']
+    else:
+        valorPatrimonialPorAcao = 0
+
+    # Divida total
+    if 'totalDebt' in stock_info:
+        dividaLiquida = stock_info['totalDebt']
+    else:
+        dividaLiquida = 9999999999
+
+    # Preço da ação
+    if 'currentPrice' in stock_info:
+        precoAtual = stock_info['currentPrice']
+    else:
+        precoAtual = 0
+
+    # Patrimonio total
+    if 'sharesOutstanding' in stock_info:
+        patrimonio = precoAtual * stock_info['sharesOutstanding']
+    else:
+        patrimonio = 0
+
+    # Indice de endividamento
+    if 'totalDebt' in stock_info and patrimonio > 0:
+        indiceDeEndividamento = dividaLiquida / patrimonio
+    else:
+        indiceDeEndividamento = 0
+
+    # Retorno sobre Ativos (ROA)
+    if 'returnOnAssets' in stock_info:
+        retornoSobreAtivos = stock_info['returnOnAssets']
+    else:
+        retornoSobreAtivos = 0
+
+    return np.array([margemDeLucro, dividendosPorAno, valorPatrimonialPorAcao, dividaLiquida, precoAtual, patrimonio, indiceDeEndividamento, retornoSobreAtivos])
 
 @app.route('/')
 @cache.cached(timeout=600) 
